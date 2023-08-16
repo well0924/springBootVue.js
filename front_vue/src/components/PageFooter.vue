62ea1ddd8bd38906d209e7323e449e8610cff766
well0924 <well414965@gmail.com>
2023-08-17 01:50:13 +0900

    feat : 게시판 목록 화면 및 레이아웃

---
+++ front_vue/src/components/PageFooter.vue
@@ -0,0 +1,17 @@
+<template>
+  <hr/>
+  <footer>
+    여기는 footer 자리입니다.
+  </footer>
+
+</template>
+
+<script>
+export default {
+  name: "PageFooter.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
